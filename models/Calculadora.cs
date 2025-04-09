c2d07b2f1486d21e516cf9cf3d4e705b6bdb9d5d
Giovanna Midonz <roboticagiovanna@gmail.com>
2025-04-09 19:37:06 -0300

    Atualizar o models
    estudo_Operadores

---
+++ models/Calculadora.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Calculadora;
+
+public class Calculadora
+{
+    public void Somar(int x, int y)
+    {
+        Console.WriteLine($"{x} e {y} = {x + y}");
+    }
+    public void Subtrar(int x, int y)
+    {
+        Console.WriteLine($"{x} e {y} = {x - y}");
+    }
+    public void Multipicar(int x, int y)
+    {
+        Console.WriteLine($"{x} e {y} = {x * y}");
+    }
+    public void Dividir(int x, int y)
+    {
+        Console.WriteLine($"{x} e {y} = {x / y}");
+    }
+}
